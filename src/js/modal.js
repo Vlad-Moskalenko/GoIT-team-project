cf8890009277731cf878af5a712e556597024ec9
Sennarion <janadine.pro@gmail.com>
2022-10-11 15:46:20 +0300

    final

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,19 +1,19 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
 
-    const scrollLockMethod = !refs.modal.classList.contains('is-hidden')
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
+//     const scrollLockMethod = !refs.modal.classList.contains('is-hidden')
+//       ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   }
+// })();
