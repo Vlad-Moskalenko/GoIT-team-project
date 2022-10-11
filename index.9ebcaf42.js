97acaa0ed74d22ba63388b094b626311d5dbf016
Vlad-Moskalenko <94741971+Vlad-Moskalenko@users.noreply.github.com>
2022-10-11 07:37:16 +0000

    Deploying to gh-pages from @ Vlad-Moskalenko/GoIT-team-project@ef11690f3876f88874c1de15d019d740355e3e0a 🚀

---
+++ index.9ebcaf42.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",slidesPerView:1,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.9ebcaf42.js.map
---
+++ index.a9d1f120.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",slidesPerView:1,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.a9d1f120.js.map
